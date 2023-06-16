481002eadce0659a54e0347ab915c0d37e3269c5
Patrick Igiraneza <pigir101@uottawa.ca>
2023-06-16 11:14:11 -0400

    feat: abstracted HTML/XML parsing

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const {DOMParser} = require('./linkedom/cjs/index.js')
+const { DOMParser } = require("./linkedom/cjs/index.js");
 
 /**
  * HTML DOM parser
  */
-class HTML{
-    /**
-     * 
-     * @param {markupLanguage} data 
-     * @returns HTMLDocument object
-     */
-    static parse(data){
-        return (new DOMParser()).parseFromString(data, 'text/html')
-    }
+class HTML {
+  /**
+   *
+   * @param {markupLanguage} data
+   * @returns HTMLDocument object
+   */
+  static parse(data) {
+    return new DOMParser().parseFromString(data, "text/html");
+  }
 }
 
 /**
  * XML DOM parser
  */
-class XML{
-    /**
-     * 
-     * @param {markupLanguage} data 
-     * @returns XMLDocument object
-     */
-    static parse(data){
-        return (new DOMParser()).parseFromString(data, 'text/xml')
-    }
+class XML {
+  /**
+   *
+   * @param {markupLanguage} data
+   * @returns XMLDocument object
+   */
+  static parse(data) {
+    return new DOMParser().parseFromString(data, "text/xml");
+  }
 }
 
 module.exports = {
-    HTML,
-    XML
-}
+  HTML,
+  XML,
+};
--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,6 @@
-const DomParser = require("../index.js")
+const { HTML } = require("../index.js");
 
-const page= `<!DOCTYPE html>
+const page = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,17 +13,23 @@ const page= `<!DOCTYPE html>
     <p>Test Paragraph</p>
     <span id="spanid">Span field</span>
 </body>
-</html>`
+</html>`;
 
-const document = new DomParser(page, "text/html")
+const document = HTML.parse(page, "text/html");
 
-function assert(state){
-    return state ? "✅" : "❌ "
+function assert(state) {
+  return state ? "✅" : "❌ ";
 }
 
-console.log(assert(document),"Dom Parsed")
-console.log(assert(document.body),"Body Parsed")
-console.log(assert(document.getElementById("spanid")),"Test getElementById()")
-console.log(assert(document.querySelector("#spanid")),"Test querySelector()")
-console.log(assert(document.querySelectorAll("span").length === 1),"Test querySelectorAll() on unique element")
-console.log(assert(document.querySelectorAll("p")[0].textContent === "Hello World"),"Test querySelectorAll() on multiple elements")
+console.log(assert(document), "Dom Parsed");
+console.log(assert(document.body), "Body Parsed");
+console.log(assert(document.getElementById("spanid")), "Test getElementById()");
+console.log(assert(document.querySelector("#spanid")), "Test querySelector()");
+console.log(
+  assert(document.querySelectorAll("span").length === 1),
+  "Test querySelectorAll() on unique element"
+);
+console.log(
+  assert(document.querySelectorAll("p")[0].textContent === "Hello World"),
+  "Test querySelectorAll() on multiple elements"
+);
