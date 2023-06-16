bb80e8d0193a5775badd252491fc04df6086cb46
Patrick Igiraneza <pigir101@uottawa.ca>
2023-06-16 10:56:58 -0400

    feat: Update index.js
    - Abstracted HTML and XML parsing into 2 different static classes

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const {DOMParser} = require("./linkedom/cjs/index.js")
+const {DOMParser} = require('./linkedom/cjs/index.js')
 
-class DomParser{
-    constructor(data, mimeType = "text/html"){
-        return (new DOMParser()).parseFromString(data, mimeType)
-        
+/**
+ * HTML DOM parser
+ */
+class HTML{
+    /**
+     * 
+     * @param {markupLanguage} data 
+     * @returns HTMLDocument object
+     */
+    static parse(data){
+        return (new DOMParser()).parseFromString(data, 'text/html')
     }
 }
 
-module.exports = DomParser
+/**
+ * XML DOM parser
+ */
+class XML{
+    /**
+     * 
+     * @param {markupLanguage} data 
+     * @returns XMLDocument object
+     */
+    static parse(data){
+        return (new DOMParser()).parseFromString(data, 'text/xml')
+    }
+}
+
+module.exports = {
+    HTML,
+    XML
+}
